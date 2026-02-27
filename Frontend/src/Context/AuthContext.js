48d4b30ad7780e45365359902193a09c0ae4700b
Shubhi49 <shubhisrivastava373@gmail.com>
2026-02-27 06:13:31 +0530

    frontend upload

---
+++ Frontend/src/Context/AuthContext.js
@@ -0,0 +1,32 @@
+import { useState, useEffect, useContext, createContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: null,
+  });
+  useEffect(() => {
+    // const data = localStorage.getItem("auth")
+    const data = sessionStorage.getItem("auth");
+    console.log("Context "+data);
+    if (data) {
+      const parsedData = JSON.parse(data);
+      setAuth({
+        user: parsedData.user,
+        token: parsedData.token,
+      });
+    }
+  }, []);
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// Custom hook
+const useAuthContext = () => useContext(AuthContext);
+
+export { useAuthContext, AuthProvider };
